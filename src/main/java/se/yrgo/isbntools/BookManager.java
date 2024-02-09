e84a87344c5ca37cd1016bba6b289ca6825a80e7
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-02-09 12:27:15 +0100

    added another book in main

--- src/main/java/se/yrgo/isbntools/BookManager.java
+++ src/main/java/se/yrgo/isbntools/BookManager.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 public class BookManager {
 
+
 	public static void main(String[] args) {
 		List<Book> books = new ArrayList<Book>();
 		books.add(new Book("069117654X", "Understanding the Digital World","Brian Kernighan"));
 		books.add(new Book("0596009658","Learning the bash Shell","Cameron Newham"));
 		books.add(new Book("0555005555","Jenkins for Beginners","Ali Maham"));
+		books.add(new Book("0555005599","Jenkins for Life","Ali Maham"));
 		for(Book book:books) {
 			System.out.println(book);
 		}
